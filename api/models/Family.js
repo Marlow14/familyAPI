db8c5310b3dd21008ad090384d632bcc814a87bc
Marlow14 <c_a_marlow14@yahoo.com>
2016-09-08 11:06:57 -0400

    updated api/model/famil.js model of family info

--- api/models/Family.js
+++ api/models/Family.js
@@ -8,6 +8,46 @@
 module.exports = {
 
   attributes: {
+  	firstName:{
+  		type:'string',
+  		minLength:2,
+  		required:true
+  	},
+  	lastName:{
+  		type:'string',
+  		minLength:2,
+  		required:true
+  	},
+  	phoneNumber:{
+  		type:'string',
+  		minLength:10,
+  		defaultsTo:'000-000-0000',
+  		regex:/^(()?\d{3}())?(-|\s)?\d{3}(-|\s)?\d{4}$/
+  		
+  	},
+  	cellPhone:{
+  		type:'string',
+  		minLength:10,
+  		regex:/^\(?([0-9{3}])\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+  	},
+  	emailAddress:{
+  		type:'string',
+  		email:true
+  	},
+  	homeAddress:{
+  		type:'string'
+  	},
+  	birthDate:{
+  		type:'string'
+  	},
+  	active:{
+  		type:'integer',
+  		enum:[1,0],
+  		defaultsTo:1,
+  		required:true
+  	}
+  	
+  	
 
   }
 };
